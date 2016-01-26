02183b3a0e9914bfd950fb9380482d0adcfe478d
Denis Savenok <denissavenok@gmail.com>
2016-01-26 13:54:15 +0400

    minor change

--- src/server/core.spec.js
+++ src/server/core.spec.js
@@ -13,9 +13,7 @@ export default {
         wildcardRoutePlugin
     ],
     app: {
-        createExpressApplication: {
-            
-        },
+        createExpressApplication: {},
         addWebpackMiddleware: {
             webpackConfig: webpackConfig
         },
