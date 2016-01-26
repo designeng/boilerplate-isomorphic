ba473c9b264def552734a48f579082eb45d7d130
Denis Savenok <denissavenok@gmail.com>
2016-01-26 16:23:01 +0400

    cleaned

--- src/server/plugins/express/falcor/middleware.js
+++ src/server/plugins/express/falcor/middleware.js
@@ -4,7 +4,6 @@ function addFalcorMiddleware(resolver, facet, wire) {
     let target = facet.target;
     const api = facet.options.api;
     api.forEach((item) => {
-        console.log(">>>>>>>", item.apiPath);
         target.use(
             item.apiPath, 
             FalcorServer.dataSourceRoute(() => new item.router)
