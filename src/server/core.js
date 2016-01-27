caeb78ede34d1d7db47ba1a7a45e8d63330ee7a6
Denis Savenok <denissavenok@gmail.com>
2016-01-27 19:02:17 +0400

    extra code removed

--- src/server/core.js
+++ src/server/core.js
@@ -1,16 +1,12 @@
 import wire from 'essential-wire';
-import coreSpec from './core.spec';
 
-import conifureStoreSpec from '../common/spec/conifure.store.spec';
+import coreSpec             from './core.spec';
+import conifureStoreSpec    from '../common/spec/conifure.store.spec';
 
 delete process.env.BROWSER;
 
-// wire(coreSpec).then((res) => {
-//     // console.log("RES:", res);
-// }).otherwise((error) => console.log("ERROR:", error));
-
 wire(conifureStoreSpec).then((context) => {
     context.wire(coreSpec).then((_context) => {
-        console.log("RES::::::", _context.middleware);
+        // console.log("RES::::::", _context);
     }).otherwise((error) => console.log("ERROR coreSpec:", error));
 }).otherwise((error) => console.log("ERROR conifureStoreSpec:", error));
