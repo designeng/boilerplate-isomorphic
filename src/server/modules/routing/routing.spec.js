496c181478e5d76970c57d32df47f3dc040d5012
designeng <denissavenok@gmail.com>
2016-01-29 14:23:56 +0300

    some npm modules added; src/server/modules folder with routing.spec.js inited

---
+++ src/server/modules/routing/routing.spec.js
@@ -0,0 +1,21 @@
+import wireDebugPlugin      from 'essential-wire/source/debug';
+
+// custom plugins
+import getCurrentUserPlugin from './plugins/user/getCurrentUserPlugin';
+
+// import { getUser }      from '../../../../common/api/user';
+
+import routes           from '../common/routes';
+
+export default {
+    $plugins: [
+        wireDebugPlugin,
+        getCurrentUserPlugin
+    ],
+    user: {
+        getUser: {}
+    }
+    expressRoutingMiddleware: {
+
+    }
+}
