bb780f17b100c423ff10ca8830ce8fb3b1476cd5
designeng <denissavenok@gmail.com>
2016-01-30 00:41:20 +0300

    commented devtools code

--- src/client/index.js
+++ src/client/index.js
@@ -4,20 +4,24 @@ import reduxSpec    from '../common/spec/redux.spec';
 import userSpec     from '../common/spec/user.spec';
 import clientSpec   from './client.spec';
 
+import devTools     from '../server/devtools'
+
 // TODO: styles should be loaded with css-modules loader later for every component
 import "../../styles/index.css";
 
 // TODO: refactor this wrapping
-// TODO: ERROR clientSpec: TypeError: __webpack_require__(...) is not a function(…)
 
 wire(reduxSpec).then(context => {
     context.wire(userSpec).then(context => {
         context.wire(clientSpec).then(context => {
-            console.log("CLIENT context store:::", context.store);
 
-            if (process.env.NODE_ENV !== 'production'){
-                require('../server/devtools')(context.store, window);
-            }
-        }).otherwise(error => console.log("ERROR clientSpec:", error));
-    }).otherwise(error => console.log("ERROR userSpec:", error));
-}).otherwise(error => console.log("ERROR reduxSpec:", error));
+            // TODO: make it work properly
+            // TODO: ERROR clientSpec: TypeError: __webpack_require__(...) is not a function(…)
+
+            // if (process.env.NODE_ENV !== 'production'){
+            //     // require('../server/devtools')(context.store, window);
+            //     devTools(context.store, window);
+            // }
+        }).otherwise(error => console.error("ERROR clientSpec:", error));
+    }).otherwise(error => console.error("ERROR userSpec:", error));
+}).otherwise(error => console.error("ERROR reduxSpec:", error));
