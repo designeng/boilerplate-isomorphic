1a33e78464153f1e33147379a028dee665df9c62
designeng <denissavenok@gmail.com>
2016-02-10 23:00:52 +0300

    store builder prettified

--- src/common/plugins/redux/storeBuilderPlugin.js
+++ src/common/plugins/redux/storeBuilderPlugin.js
@@ -2,50 +2,61 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createLogger from 'redux-logger';
 
 function getStore(resolver, compDef, wire) {
-    wire(compDef.options).then(options => {
-        const storeBuilder      = options.storeBuilder;
-        const user              = options.user;
-
+    wire(compDef.options).then( ({
+        storeBuilder,
+        user
+    }) => {
         let store = storeBuilder({user: user});
         resolver.resolve(store);
     });
+}
+
+function getPlatformMiddleware(middleware) {
+    let universalMiddleware = middleware.universal;
+
+    if (process.browser) {
+        if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
+            /* Production */
+            return [
+                applyMiddleware(...universalMiddleware)
+            ].concat(middleware.browser.production)
+        } else {
+            /* Development */
+            return [
+                applyMiddleware(...universalMiddleware, createLogger())
+            ].concat(middleware.browser.development)
+        }
+    } else {
+        /* Server Side */
+        return [
+            applyMiddleware(...universalMiddleware)
+        ].concat(middleware.server)
+    }
 }
 
 // options
 //      rootReducer,
 //      middleware
 function getStoreBuilder(resolver, compDef, wire) {
-    const getPlatformMiddleware = (middleware) => {
-        let universalMiddleware = middleware.universal;
+    wire(compDef.options).then( ({ 
+            rootReducer,
+            middleware
+    }) => {
+        const finalCreateStore = compose(...getPlatformMiddleware(middleware))(createStore);
+        const storeBuilder = (initialState) => finalCreateStore(rootReducer, initialState);
 
-        if (process.browser) {
-            if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
-                /* Production */
-                return [
-                    applyMiddleware(...universalMiddleware)
-                ].concat(middleware.browser.production)
-            } else {
-                /* Development */
-                return [
-                    applyMiddleware(...universalMiddleware, createLogger())
-                ].concat(middleware.browser.development)
-            }
-        } else {
-            /* Server Side */
-            return [
-                applyMiddleware(...universalMiddleware)
-            ].concat(middleware.server)
+        resolver.resolve(storeBuilder);
+    })
+}
+
+export default function storeBuilderPlugin(options) {
+    return {
+        factories: {
+            getStoreBuilder,
+            getStore
         }
     }
-    
-    wire(compDef.options).then((options) => {
-        const rootReducer   = options.rootReducer;
-        // {Object} middleware - {universal:..., browser:..., server:...}
-        const middleware    = options.middleware;
-
-        const finalCreateStore = compose(...getPlatformMiddleware(middleware))(createStore);
-        
-        const storeBuilder = (initialState) => finalCreateStore(rootReducer, initialState);
+}   const storeBuilder = (initialState) => finalCreateStore(rootReducer, initialState);
 
         resolver.resolve(storeBuilder);
     })
