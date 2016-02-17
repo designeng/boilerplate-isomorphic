697b404df2b1a08c6be1596618549388b912fc96
designeng <denissavenok@gmail.com>
2016-02-17 10:57:26 +0300

    with when/pipeline

--- src/server/core.js
+++ src/server/core.js
@@ -1,4 +1,5 @@
 import wire from 'essential-wire';
+import pipeline from 'when/pipeline';
 
 import reduxSpec    from '../common/spec/redux.spec';
 import userSpec     from '../common/spec/user.spec';
@@ -8,12 +9,27 @@ import usersActivity    from './noop/users/activity';
 
 delete process.env.BROWSER;
 
+// wire(reduxSpec).then(context => {
+//     context.wire(userSpec).then(context => {
+//         context.wire(coreSpec).then(context => {
+//             usersActivity(context.socketIo);
+//         }).otherwise(error => console.error("ERROR coreSpec:", error));
+//     }).otherwise(error => console.error("ERROR userSpec:", error));
+// }).otherwise(error => console.error("ERROR reduxSpec:", error));
 
-// TODO: refactor this wrapping!
-wire(reduxSpec).then(context => {
-    context.wire(userSpec).then(context => {
-        context.wire(coreSpec).then(context => {
-            usersActivity(context.socketIo);
-        }).otherwise(error => console.error("ERROR coreSpec:", error));
-    }).otherwise(error => console.error("ERROR userSpec:", error));
-}).otherwise(error => console.error("ERROR reduxSpec:", error));
+
+const first = () => {
+    return wire(reduxSpec);
+}
+
+const second = (context) => {
+    return context.wire(userSpec);
+}
+
+const third = (context) => {
+    return context.wire(coreSpec);
+}
+
+pipeline([first, second, third]).then(context => {
+    usersActivity(context.socketIo);
+}).otherwise(error => console.error("ERROR in coreSpec:", error));
