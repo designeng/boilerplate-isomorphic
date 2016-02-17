0bb6c50cdd7696aa7416ca9bdcc9d4b15850fc66
designeng <denissavenok@gmail.com>
2016-02-17 14:02:03 +0300

    commented

--- src/common/components/Chat.js
+++ src/common/components/Chat.js
@@ -37,7 +37,7 @@ class Chat extends Component {
             userId: 'John'
         }
         // optimistic update
-        this.props.messageAdd(Object.assign({}, {presentMessages: this.props.messages, message: Object.assign({message: message.message + " OPT"})}));
+        // this.props.messageAdd(Object.assign({}, {presentMessages: this.props.messages, message: Object.assign({message: message.message + " OPT"})}));
         // send message to server
         this.props.messageSend(message);
     }
